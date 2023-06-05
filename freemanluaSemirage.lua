008384d345463a2121ae000b602c65e2cf1965b4
southstar <75630937+IamUndetected@users.noreply.github.com>
2023-06-05 21:26:39 +0200

    Update freemanluaSemirage.lua

--- freemanluaSemirage.lua
+++ freemanluaSemirage.lua
@@ -1,82 +1,64 @@
---[[
-Freeeman Semirage
-
-Created by: mrfreeman
-Created on the: 4th of June 2023
-Created for: Learning lua and giving the community a free lua
-
-
-Yes i know that most of this is shit code, yes i know that a lot of it is pasted but yk i guess it works? :)
---]]
-
-
-
---[[
-STRUCTURE:
-
-1. Setups
-
-
-
-
---]]
-
-
---Setups
-local ECLIB = require "gamesense/engineclient" --  https://gamesense.pub/forums/viewtopic.php?id=42362
-local images = require "gamesense/images" --  https://gamesense.pub/forums/viewtopic.php?id=22917
+local ffi = require "ffi"
 local js = panorama.open()
 
+local engineclient = require "gamesense/engineclient" or error "https://gamesense.pub/forums/viewtopic.php?id=42362" -- EngineClient Interface by h4x0r1337 (https://gamesense.pub/forums/profile.php?id=12351)
+local images = require "gamesense/images" or error "https://gamesense.pub/forums/viewtopic.php?id=22917" -- Images library by sapphyrus (https://gamesense.pub/forums/profile.php?id=561)
 
-local lua_name = "freeman.lua"
+local name, xuid = js.MyPersonaAPI.GetName(), js.MyPersonaAPI.GetXuid()
+local avatar = images.get_steam_avatar(xuid)
 
-local user = entity.get_player_name(entity.get_local_player())
+local tab, container = "LUA", "B"
+local master_switch = ui.new_checkbox(tab, container, "Freeman - Semirage")
 
-local build = "dev"
-local tab, container = "MISC", "Settings"
-
-
-local welcomeText = string.format("Welcome back %s, %s is now loaded. You are currently on build type: %s", user, lua_name, build)
-print(welcomeText)
-
-
---Setupps DONE
-
-
---Get Watermark stuff for future
-
-local steamid64 = js.MyPersonaAPI.GetXuid()
-local avatar = images.get_steam_avatar(steamid64)
-
-
---Watermark stuff done.
-
-local label = ui.new_label(tab, container, "Freeman.lua - Semirage")
-
-local refs = {
-    awall = ui.reference('rage', 'other', 'automatic penetration'),
-    ashoot = ui.reference('rage', 'other', 'automatic fire')
-}
+local cache = {}
 
 local menu = {
-    awallBind = ui.new_hotkey ("MISC", "Settings", 'Automatic penetration'),
-    ashootBind = ui.new_hotkey ("MISC", "Settings", 'automatic fire')
+    automatic_fire = ui.new_hotkey(tab, container, "Automatic fire", false),
+    automatic_penetration = ui.new_hotkey(tab, container, "Automatic penetration", false)
 }
 
-client.set_event_callback('setup_command', function ()
-    ui.set(refs.awall, ui.get(menu.awallBind))
-    ui.set(refs.ashoot, ui.get(menu.ashootBind))
-end)
+local references = {
+    automatic_fire = ui.reference("RAGE", "Other", "Automatic fire"),
+    automatic_penetration = ui.reference("RAGE", "Other", "Automatic penetration")
+}
 
+local paint = function(ctx)
+    local local_player = entity.get_local_player()
+    if local_player == nil or not entity.is_alive(local_player) then return end
 
-
-client.set_event_callback("paint", function()
-
-    if ui.get(refs.awall) == true then
-        renderer.indicator(255,255,255,255, "Automatic Penetration")
+    if cache[references.automatic_fire] then
+        renderer.indicator(255, 255, 255, 240, "AF")
     end
 
-    if ui.get(refs.ashoot) == true then
-        renderer.indicator(255,255,255,255, "Automatic Fire")
+    if cache[references.automatic_penetration] then
+        renderer.indicator(255, 255, 255, 240, "AW")
     end
-end)
+end
+
+local run_command = function(cmd)
+    ui.set(references.automatic_fire, ui.get(menu.automatic_fire))
+    ui.set(references.automatic_penetration, ui.get(menu.automatic_penetration))
+end
+
+local automatic_fire = function(self) cache[self] = ui.get(self) end
+local automatic_penetration = function(self) cache[self] = ui.get(self) end
+
+local ui_callback = function(self)
+    local enabled = ui.get(self)
+    local updatecallback = enabled and client.set_event_callback or client.unset_event_callback
+
+    for k, v in pairs(menu) do ui.set_visible(v, enabled) end
+
+    updatecallback("paint", paint)
+    updatecallback("run_command", run_command)
+end
+
+ui.set_callback(master_switch, ui_callback)
+ui.set_callback(references.automatic_fire, automatic_fire)
+ui.set_callback(references.automatic_penetration, automatic_penetration)
+
+ui_callback(master_switch)
+automatic_fire(references.automatic_fire)
+automatic_penetration(references.automatic_penetration)
+
+print(string.format("Welcome back %s, freeman-semirage is now loaded. you're currently on the %s build", name, "XAXA"))
